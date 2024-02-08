927fe440be552808df1424e7911e57ca3827d3ec
t0nn1x <antoonhrobust@gmail.com>
2024-02-08 20:45:40 +0100

    Initial commit

---
+++ src/main/java/org/t0nn1x/projectmanagement/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package org.t0nn1x.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectManagementApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/t0nn1x/projectmanagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.t0nn1x.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
